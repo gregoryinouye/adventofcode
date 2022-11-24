90bc55ee61a2ee3b0db3eb89932a4b8aa19eac3c
Gregory Inouye <48232124+gregoryinouye@users.noreply.github.com>
2022-11-24 03:01:00 -0800

    add 2015-06 python solution

---
+++ 2015/06/2015-06.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+
+import hashlib
+from pathlib import Path
+import re
+import sys
+
+filepath = Path(__file__)
+test_filename = filepath.stem.split('_')[0] + '.txt'
+test_filepath = filepath.parent / test_filename
+part_one_answer = 569999
+part_two_answer = 17836115
+
+
+def parse(input_path: Path) -> list[str]:
+    return input_path.read_text().strip().split('\n')
+
+
+def part_one(instructions: list[str]) -> int:
+    lights = [[0] * 1000 for _ in range(1000)]
+
+    for instruction in instructions:
+        command, x1_str, y1_str, x2_str, y2_str = re.sub('turn | through', '', instruction).replace(',', ' ').split(' ')
+
+        for x in range(int(x1_str), int(x2_str) + 1):
+            for y in range(int(y1_str), int(y2_str) + 1):
+                if command == 'off':
+                    lights[x][y] = 0
+                elif command == 'on':
+                    lights[x][y] = 1
+                elif command == 'toggle':
+                    lights[x][y] = int(not lights[x][y])
+                else:
+                    raise Exception('unknown command')
+
+    return sum(sum(row) for row in lights)
+
+
+def part_two(instructions: list[str]) -> int:
+    lights = [[0] * 1000 for _ in range(1000)]
+
+    for instruction in instructions:
+        command, x1_str, y1_str, x2_str, y2_str = re.sub('turn | through', '', instruction).replace(',', ' ').split(' ')
+
+        for x in range(int(x1_str), int(x2_str) + 1):
+            for y in range(int(y1_str), int(y2_str) + 1):
+                if command == 'off':
+                    lights[x][y] = lights[x][y] - 1 if lights[x][y] > 0 else 0
+                elif command == 'on':
+                    lights[x][y] += 1
+                elif command == 'toggle':
+                    lights[x][y] += 2
+                else:
+                    raise Exception('unknown command')
+
+    return sum(sum(row) for row in lights)
+
+
+if __name__ == '__main__':
+    paths = [test_filename] if len(sys.argv) <= 1 else sys.argv[1:]
+
+    for path_str in paths:
+        print(f"\n{path_str}:")
+        puzzle_input = parse(Path(path_str))
+
+        part_one_actual = part_one(puzzle_input)
+        part_two_actual = part_two(puzzle_input)
+
+        result_one = '\u2705 ' if part_one_actual == part_one_answer else '\u274C'
+        result_two = '\u2705 ' if part_two_actual == part_two_answer else '\u274C'
+
+        print(result_one, 'Part One:', part_one_actual)
+        print(result_two, 'Part Two:', part_two_actual)
---
+++ 2015/06/2015-06_test.py
@@ -0,0 +1,35 @@
+import pathlib
+import pytest
+import importlib
+
+
+filepath = pathlib.Path(__file__)
+day = filepath.stem.split('_')[0]
+solution = importlib.import_module(day)
+
+
+@pytest.fixture
+def test_input():
+    return solution.parse(solution.test_filepath)
+
+
+def test_part_one_with_example_input1():
+    assert solution.part_one(['turn on 0,0 through 999,999']) == 1_000_000
+
+
+def test_part_one_with_example_input2():
+    assert solution.part_one(['toggle 0,0 through 999,0']) == 1_000
+
+
+def test_part_one_with_example_input3():
+    assert solution.part_one(['turn on 0,0 through 999,999', 'turn off 499,499 through 500,500']) == 999_996
+
+
+@pytest.mark.skip
+def test_part_one(test_input):
+    assert solution.part_one(test_input) == solution.part_one_answer
+
+
+@pytest.mark.skip
+def test_part_two(test_input):
+    assert solution.part_two(test_input) == solution.part_two_answer
