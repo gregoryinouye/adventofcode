80c08b013fff919484bf15b9cc1106be8ee42920
Gregory Inouye <48232124+gregoryinouye@users.noreply.github.com>
2022-11-24 09:50:27 -0800

    add space to test result print

--- 2015/06/2015-06.py
+++ 2015/06/2015-06.py
@@ -66,8 +66,8 @@ if __name__ == '__main__':
         part_one_actual = part_one(puzzle_input)
         part_two_actual = part_two(puzzle_input)
 
-        result_one = '\u2705 ' if part_one_actual == part_one_answer else '\u274C'
-        result_two = '\u2705 ' if part_two_actual == part_two_answer else '\u274C'
+        result_one = '\u2705 ' if part_one_actual == part_one_answer else '\u274c '
+        result_two = '\u2705 ' if part_two_actual == part_two_answer else '\u274c '
 
         print(result_one, 'Part One:', part_one_actual)
         print(result_two, 'Part Two:', part_two_actual)
