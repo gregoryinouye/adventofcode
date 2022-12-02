be8b7f9400588a95d0af4cb37608d59ff1e5b743
Gregory Inouye <48232124+gregoryinouye@users.noreply.github.com>
2022-12-02 11:43:52 -0800

    add 2022-01

---
+++ 2022/01/2022-01.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+import sys
+from pathlib import Path
+
+filepath = Path(__file__)
+test_filename = filepath.stem + '.txt'
+test_filepath = filepath.parent / test_filename
+part_one_answer = 72017
+part_two_answer = 212520
+
+
+def parse(input_path: Path) -> str:
+    return input_path.read_text().strip()
+
+
+def part_one(lines: str) -> int:
+    lines_split = lines.split('\n\n')
+    elves = [line.split('\n') for line in lines_split]
+    elves_calories = [sum(map(int, items)) for items in elves]
+
+    return sorted(elves_calories)[-1]
+
+
+def part_two(lines: str) -> int:
+    lines_split = lines.split('\n\n')
+    elves = [line.split('\n') for line in lines_split]
+    elves_calories = [sum(map(int, items)) for items in elves]
+
+    return sum(sorted(elves_calories)[-3:])
+
+
+if __name__ == '__main__':
+    paths = [test_filename] if len(sys.argv) <= 1 else sys.argv[1:]
+
+    for path_str in paths:
+        print(f"\n{path_str}:")
+        puzzle_input = parse(Path(path_str))
+
+        part_one_actual = part_one(puzzle_input)
+        part_two_actual = part_two(puzzle_input)
+
+        result_one = '\u2705 ' if part_one_actual == part_one_answer else '\u274c '
+        result_two = '\u2705 ' if part_two_actual == part_two_answer else '\u274c '
+
+        print(result_one, 'Part One:', part_one_actual)
+        print(result_two, 'Part Two:', part_two_actual)
