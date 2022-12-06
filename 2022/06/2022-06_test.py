f9ac3020eecf112b58ed3f012d17a348bc6dfcf3
Gregory Inouye <48232124+gregoryinouye@users.noreply.github.com>
2022-12-06 07:48:23 -0800

    add 2022-06

---
+++ 2022/06/2022-06_test.py
@@ -0,0 +1,46 @@
+import pathlib
+import pytest
+import importlib
+
+filepath = pathlib.Path(__file__)
+day = filepath.stem.split('_')[0]
+solution = importlib.import_module(day)
+
+
+@pytest.fixture
+def test_input():
+    return solution.parse(solution.test_filepath)
+
+
+@pytest.fixture
+def test_example_input():
+    return 'bvwbjplbgvbhsrlpgdmjqwftvncz'
+
+
+@pytest.mark.parametrize('test_input,expected', [
+    ('bvwbjplbgvbhsrlpgdmjqwftvncz', 5),
+    ('nppdvjthqldpwncqszvftbrmjlhg', 6),
+    ('nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg', 10),
+    ('zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw', 11)
+])
+def test_part_one_with_example_input(test_input, expected):
+    assert solution.part_one(test_input) == expected
+
+
+@pytest.mark.parametrize('test_input,expected', [
+    ('mjqjpqmgbljsphdztnvjfqwrcgsmlb', 19),
+    ('bvwbjplbgvbhsrlpgdmjqwftvncz', 23),
+    ('nppdvjthqldpwncqszvftbrmjlhg', 23),
+    ('nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg', 29),
+    ('zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw', 26)
+])
+def test_part_two_with_example_input(test_input, expected):
+    assert solution.part_two(test_input) == expected
+
+
+def test_part_one(test_input):
+    assert solution.part_one(test_input) == solution.part_one_answer
+
+
+def test_part_two(test_input):
+    assert solution.part_two(test_input) == solution.part_two_answer
