71063696476f2283f41bda8c5c79dc6d5f63b524
gregoryinouye <48232124+gregoryinouye@users.noreply.github.com>
2024-12-20 22:33:22 -0800

    add 2023-07 template

---
+++ 2023/07/2023-07_test.py
@@ -0,0 +1,39 @@
+import pathlib
+import pytest
+import importlib
+
+filepath = pathlib.Path(__file__)
+day = filepath.stem.split('_')[0]
+solution = importlib.import_module(day)
+
+
+@pytest.fixture
+def test_input():
+    return solution.parse(solution.test_filepath)
+
+
+@pytest.fixture
+def test_example_input():
+    return [
+        '32T3K 765',
+        'T55J5 684',
+        'KK677 28',
+        'KTJJT 220',
+        'QQQJA 483',
+    ]
+
+
+def test_part_one_with_example_input(test_example_input):
+    assert solution.part_one(test_example_input) == -1
+
+
+def test_part_two_with_example_input(test_example_input):
+    assert solution.part_two(test_example_input) == -1
+
+
+def test_part_one(test_input):
+    assert solution.part_one(test_input) == solution.part_one_answer
+
+
+def test_part_two(test_input):
+    assert solution.part_two(test_input) == solution.part_two_answer
